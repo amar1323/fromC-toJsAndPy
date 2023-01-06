014fbc1b2003f3fbc83558e1d7dfc039ce6b38e1
amar1323 <66776149+amar1323@users.noreply.github.com>
2023-01-06 21:52:59 +0100

    Delete problems/easy/110.BalancedBinaryTree/cpp/build directory

--- problems/easy/110.BalancedBinaryTree/cpp/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/amarelouazanituhami/Dev/fromC-toJsAndPy/problems/easy/110.BalancedBinaryTree/cpp
-# Build directory: /Users/amarelouazanituhami/Dev/fromC-toJsAndPy/problems/easy/110.BalancedBinaryTree/cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
